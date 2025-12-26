102807e9b43557de9c439dc84405934f4b7a79d2
Farash ibra <farassrl@gmail.com>
2025-12-26 23:06:30 +0700

    initiate commit

--- backend/server.js
+++ backend/server.js
@@ -52,5 +52,5 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-  console.log(`API listening on http://localhost:${PORT}`);
+  console.log(`API listening on port ${PORT}`);
 });
